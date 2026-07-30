3ae368524c5ac3eb429e755fde54ee0c1933550f
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-07-30 23:50:55 +0530

    candies leetcode version

--- 1431.py
+++ 1431.py
@@ -1,10 +1,12 @@
 x = list(map(int, input("Enter: ").split()))
 y = int(input("Enter: "))
-def kids(x,y,m):
+def kids(x,y):
     m = max(x)
+    result = []
     for i in range(len(x)):
-        if (x[i] + y >= m):
-            print("true", end = " ")
+        if (x[i] + y) >= m:
+            result.append(True)
         else:
-            print("false", end = " ")
-kids(x,y,m)
+            result.append(False)
+    return result
+print(kids(x,y))
