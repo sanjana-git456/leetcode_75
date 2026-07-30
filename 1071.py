aec9ba6b265e335fa5e945d67d6785aaa75931bb
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-07-30 23:52:05 +0530

    gcd of string

--- 1071.py
+++ 1071.py
@@ -1,9 +1,9 @@
 import math
-x = input("Enter: ")
-y = input("Enter: ")
+str1 = input("Enter: ")
+str2= input("Enter: ")
 def gcdOfStrings(str1, str2):
     if str1+str2 != str2+str1:
         return ""
     g = math.gcd(len(str1), len(str2))
     return str1[:g]
-print(gcdOfStrings(x,y))
+print(gcdOfStrings(str1,str2))
