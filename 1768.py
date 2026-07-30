6b03e651236c0f5f4c8e69c29c1c81412dd133f2
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-07-30 23:53:52 +0530

    alternate letters in one word

--- 1768.py
+++ 1768.py
@@ -1,10 +1,10 @@
 word1 = input("Enter: ")
 word2 = input("Enter: ")
 l = []
-t = min(len(word1), len(word2))
-for i in range(t):
+total = min(len(word1), len(word2))
+for i in range(total):
     l.append(word1[i])
     l.append(word2[i])
-l.append(word1[t:])
-l.append(word2[t:])
+l.append(word1[total:])
+l.append(word2[total:])
 print(''.join(l))
