73695a389da4ceb33c892a968be243c67c1b80d9
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-08-03 23:12:05 +0530

    corrected pairs

--- 1679.py
+++ 1679.py
@@ -4,13 +4,15 @@ def pair(x,t):
     c = 0
     d = {}
     for i in range(len(x)):
+        done = False
         a = t - x[i]
-        if a in d:
+        if a in d and d[a] > 0:
             d[a] -= 1
             c += 1
-        if x[i] not in d:
+            done = True
+        if x[i] not in d and done == False:
             d[x[i]] = 1
-        else:
+        elif done == False:
             d[x[i]] += 1
     return c
 print(pair(x,t))
