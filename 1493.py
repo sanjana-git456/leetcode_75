2b59bcd38736eb3163c5beeb260cefe486de68de
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-08-08 22:43:11 +0530

    remove one element longest with 1s

---
+++ 1493.py
@@ -0,0 +1,15 @@
+x = list(map(int, input("Enter: ").split()))
+def one(x):
+    left = 0
+    ans = 0
+    right = 0
+    c = 0
+    for right in range(len(x)):
+        if x[right] == 0:
+            c += 1
+        while c > 1:
+            left += 1
+            c -= 1
+        ans = max(ans, right-left)
+    return ans
+print(one(x))
