7ff6cb34f1ab41bb3b60b90de71037198f0562bb
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-08-09 19:47:07 +0530

    common elements better

--- 2215.py
+++ 2215.py
@@ -3,16 +3,6 @@ y = list(map(int, input("Enter: ").split()))
 def common(x,y):
     x = set(x)
     y = set(y)
-    ans = []
-    a = []
-    b = []
-    for i in x:
-        if i not in y:
-            a.append(i)
-    ans.append(a)
-    for j in y:
-        if j not in x:
-            b.append(j)
-    ans.append(b)
+    ans = [list(x-y),list(y-x)]
     return ans
 print(common(x,y))
