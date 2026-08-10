7b0fa2d8411a2ea43bafc1aadb0cc741680aec28
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-08-10 23:14:30 +0530

    equal row column pairs

--- 2352.py
+++ 2352.py
@@ -3,14 +3,14 @@ def rc(x):
     r = []
     c = []
     count = 0
-    for i in range(3):
+    for i in range(len(x)):
         a = []
         r.append(x[i])
-        for j in range(3):
+        for j in range(len(x)):
             a.append(x[j][i])
         c.append(a)
-    for i in range(3):
-        for j in range(3):
+    for i in range(len(x)):
+        for j in range(len(x)):
             if r[i] == c[j]:
                 count += 1
     return count
