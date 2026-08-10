1b5b48ff81f412d26580ff6efb50e0ab94d26a69
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-08-11 00:11:52 +0530

    asteroids

--- 735.py
+++ 735.py
@@ -2,7 +2,18 @@ x = list(map(int, input("Enter: ").split()))
 def asteroid(x):
     l = [x[0]]
     for i in range(1,len(x)):
-        if l[-1] > 0 and x[i] < 0:
-            l.append(max(abs(x[i]),abs(l[-1])))
-        elif l[-1] > 0 and x[i] < 0 and abs(l[-1]) == abs(x[i]):
-            l.pop()
+        alive = True
+        while l and l[-1] > 0 and x[i] < 0:
+            if abs(l[-1]) == abs(x[i]):
+                l.pop()
+                alive = False
+                break
+            elif abs(l[-1]) < abs(x[i]):
+                l.pop()
+            elif abs(l[-1]) > abs(x[i]):
+                alive = False
+                break
+        if alive:
+            l.append(x[i])
+    return l
+print(asteroid(x))
