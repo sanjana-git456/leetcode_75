d6cadfa91db9399754ff3de5162f50096a1128f7
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-08-12 22:57:37 +0530

    recent calls

--- 933.py
+++ 933.py
@@ -5,9 +5,9 @@ class RecentCounter:
 
     def ping(self, t: int) -> int:
         self.requests.append(t)
-        valid []
+        valid = []
         for i in self.requests:
             if i >= t-3000:
                 valid.append(i)
-            self.requests = valid
+        self.requests = valid
             return len(self.requests)
