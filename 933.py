a0afc960cb56c3d57ff06c8d392efbef11c3ba1f
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-08-12 22:56:27 +0530

    recent calls

---
+++ 933.py
@@ -0,0 +1,13 @@
+class RecentCounter:
+
+    def __init__(self):
+        self.requests = []
+
+    def ping(self, t: int) -> int:
+        self.requests.append(t)
+        valid []
+        for i in self.requests:
+            if i >= t-3000:
+                valid.append(i)
+            self.requests = valid
+            return len(self.requests)
