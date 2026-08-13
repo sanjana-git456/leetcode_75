e231b8226a9b2a77718e41d9fa36f347531ef044
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-08-14 01:07:56 +0530

    product except itself

--- Except.java
+++ Except.java
@@ -3,13 +3,12 @@ import java.util.*;
 
 public class Except {
 
-    public int[] exc(int[] x) {
+    public ArrayList<Integer> exc(int[] x) {
         ArrayList<Integer> al = new ArrayList<>();
         for (int i = 0; i < x.length; i++) {
-            int fix = x[i];
             int p = 1;
             for (int j = 0; j < x.length; j++) {
-                if (x[j] != fix) {
+                if (j != i) {
                     p *= x[j];
                 }
             }
