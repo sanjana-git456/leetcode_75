7175c0674a9990f32eb528f687a74aa1b5abee2a
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-08-15 17:01:44 +0530

    move zeros to the end

--- MoveZeros.java
+++ MoveZeros.java
@@ -1,7 +1,7 @@
 
 public class MoveZeros {
 
-    public char[] zero(char[] x) {
+    public int[] zero(int[] x) {
         int write = 0;
         for (int i = 0; i < x.length; i++) {
             if (x[i] != 0) {
@@ -13,5 +13,10 @@ public class MoveZeros {
             x[i] = 0;
         }
         return x;
+    }
+    public static void  main(String[] args) {
+        MoveZeros mz = new MoveZeros();
+        int[] arr = {1,5,0,2,0,4,7,0,6};
+        System.out.println(mz.zero(arr));
     }
 }
