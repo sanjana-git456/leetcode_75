25d3ef7e41317b9785572038306f7249ee73f572
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-08-16 23:05:54 +0530

    dota

--- 649.py
+++ 649.py
@@ -3,9 +3,18 @@ x = input("Enter: ")
 def dota(x):
     r = deque()
     d = deque()
+    n = len(x)
     for i in range(len(x)):
         if x[i] == "R":
             r.append(i)
         else:
             d.append(i)
-        
+    while r and d:
+        if r[0] < d[0]:
+            d.popleft()
+            winner = r.popleft()
+            r.append(winner+n)
+        else:
+            r.popleft()
+            winner = d.popleft()
+            d.append(winner+n)
