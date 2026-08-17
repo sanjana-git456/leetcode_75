333b9c29f2fa0cd2012212eec94eaf6c76038e4e
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-08-17 19:36:04 +0530

    deleting middle node

--- 2095.py
+++ 2095.py
@@ -16,8 +16,18 @@ def middle(head):
     slow.next = None
     return head
 
+def printnode(head):
+    vals = []
+    while head:
+        vals.append(head.val)
+        head = head.next
+    print(vals)
+
 head = Node(1)
 head.next = Node(2)
 head.next.next = Node(3)
 head.next.next.next = Node(4)
-print(middle(head))
+head.next.next.next.next = Node(5)
+
+result = middle(head)
+printnode(result)
