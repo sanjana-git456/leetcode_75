1c2de01163d58c4a83a719ca774b56ddf1071153
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-08-19 23:56:31 +0530

    odd even linked list

--- 328.py
+++ 328.py
@@ -14,10 +14,10 @@ def oddeven(head):
     even = head.next
     evenhead = even
     while even and even.next:
-        even.next = even.next.next
         odd.next = odd.next.next
-        even = even.next
+        even.next = even.next.next
         odd = odd.next
+        even = even.next
     odd.next = evenhead
     return head
 def printnode(head):
